b2afaf9e2b9433e78edfb2922a05b0d712b49fad
Kim Brandwijk <kim.brandwijk@gmail.com>
2017-11-19 05:41:12 +0100

    Added multiple-schemas example

---
+++ examples/multiple-schemas/index.ts
@@ -0,0 +1,79 @@
+require('dotenv').config()
+
+import * as express from 'express'
+import expressPlayground from 'graphql-playground-middleware-express'
+import { remoteSchema, schema, resolver, serve, use } from 'qewl'
+
+async function run() {
+
+  const app = express()
+  const graphql = express.Router()
+
+  // Schemas
+  graphql.use(
+    remoteSchema({uri: process.env.GRAPHCOOL_ADDRESS_ENDPOINT ||
+      'https://api.graph.cool/simple/v1/cj97mysum1jyi01363osr460n'}),
+
+    remoteSchema({uri: process.env.GRAPHCOOL_WEATHER_ENDPOINT ||
+      'https://api.graph.cool/simple/v1/cj97mrhgb1jta01369lzb0tam'}),
+
+    schema(`
+      extend type Address {
+        weather: WeatherPayload
+      }
+
+      extend type WeatherPayload {
+        temp(unit: UnitEnum): Float
+      }
+
+      enum UnitEnum {
+        Celcius,
+        Fahrenheit
+      }`
+    )
+  )
+
+  // Resolvers
+  graphql.use(
+    resolver('Address.weather', {
+      fragment: `fragment AddressFragment on Address { city }`,
+      resolve: (event) => {
+        const { city } = event.parent
+        return event.delegate('query', 'getWeatherByCity', { city })
+      }
+    }),
+
+    resolver('WeatherPayload.temp', {
+      fragment: `fragment WeatherPayloadFragment on WeatherPayload { temperature }`,
+      resolve: (event) => {
+        const { temperature } = event.parent
+        switch (event.args.unit) {
+          case 'Fahrenheit':
+            return temperature * 1.8 + 32
+          default:
+            return temperature
+        }
+      }
+    })
+  )
+
+  // Middleware
+  graphql.use(
+    use('User.name', async (event, next) => {
+      if (!event.context.headers.scopes || ''.includes('read:username')) {
+        throw new Error('Insufficient permissions')
+      } else {
+        return await next()
+      }
+    })
+  )
+
+  app.use('/graphql', express.json(), graphql, await serve())
+  app.use('/playground', expressPlayground({ endpoint: '/graphql' }))
+
+  app.listen(3000, () =>
+    console.log('Server running. Open http://localhost:3000/playground to run queries.')
+  )
+}
+
+run().catch(console.error.bind(console))
