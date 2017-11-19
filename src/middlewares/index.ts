b9cc0c541e0d2813914fa3933c22e1b3742ac316
Kim Brandwijk <kim.brandwijk@gmail.com>
2017-11-20 00:39:07 +0100

    Added middleware for specifying multiple resolvers

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,7 +1,7 @@
 export { base } from './base'
 export { schema } from './schema'
 export { remoteSchema } from './remoteSchema'
-export { resolver } from './resolver'
+export { resolver, resolvers } from './resolver'
 export { serve } from './serve'
 export { use } from './use'
 export { generateSchema } from './generateSchema'
--- src/middlewares/resolver.ts
+++ src/middlewares/resolver.ts
@@ -18,3 +18,25 @@ export function resolver(
     next()
   })
 }
+
+export function resolvers(def: {
+  [key: string]: {
+    [key: string]: QewlRouterResolver | ((event: QewlRouterEvent) => Promise<any> | any)
+  }
+}): AsyncRequestHandler {
+  return base(async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    // Guard
+    for (const parent of Object.keys(def)) {
+      for (const field of Object.keys(def[parent])) {
+        const path = `${parent}.${field}`
+        if (req.qewl.resolvers.some(r => r.path === path)) {
+          throw new Error(`${path}: You can only specify one resolver for a path`)
+        }
+
+        req.qewl.resolvers.push({ path, resolver: def[parent][field] })
+      }
+    }
+
+    next()
+  })
+}
