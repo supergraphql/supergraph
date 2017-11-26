e235daa8d7f350089e26038cd7323fdcf23e9a24
Kim Brandwijk <kim.brandwijk@gmail.com>
2017-11-26 06:48:27 +0100

    Full control over final schema

--- src/middlewares/transform.ts
+++ src/middlewares/transform.ts
@@ -1,26 +1,6 @@
 import { Request, Response, NextFunction, AsyncRequestHandler } from 'express'
 import { base } from './base'
-import {
-  INTERFACE_TYPE_DEFINITION,
-  OBJECT_TYPE_DEFINITION,
-  ENUM_TYPE_DEFINITION,
-  UNION_TYPE_DEFINITION,
-  INPUT_OBJECT_TYPE_DEFINITION
-} from 'graphql/language/kinds'
-import {
-  parse,
-  GraphQLObjectType,
-  GraphQLInterfaceType,
-  GraphQLFieldMap,
-  GraphQLEnumType,
-  GraphQLEnumValue,
-  GraphQLUnionType,
-  GraphQLInputObjectType,
-  GraphQLInputFieldMap,
-  TypeDefinitionNode,
-  GraphQLSchema
-} from 'graphql'
-import { remove } from 'lodash'
+import { parse, TypeDefinitionNode, GraphQLSchema } from 'graphql'
 import { get, put } from 'memory-cache'
 import { mergeSchemas } from 'graphql-tools'
 
@@ -29,67 +9,86 @@ export function transform(finalSchema: string): AsyncRequestHandler {
     req.qewl.on('schemaGenerated', generatedSchema => {
       if (!get('qewl.finalSchema')) {
         const doc = parse(finalSchema)
-        const schemaDef: GraphQLSchema = mergeSchemas({ schemas: [req.qewl.schemas.mergedSchema] }) as GraphQLSchema
-        for (const definition of doc.definitions) {
-          const name = (definition as TypeDefinitionNode).name.value
-          const typeMapEntry: any = schemaDef.getTypeMap()[name]
-          if (!typeMapEntry) {
-            continue
-          }
 
-          let fieldNames: Array<string> = []
-          let fields: any
-          let keysFn: any
-          let deleteFn: (coll: any, item: string) => void = (coll, item) => {
-            /* */
-          }
-          switch (definition.kind) {
-            case OBJECT_TYPE_DEFINITION:
-            case INTERFACE_TYPE_DEFINITION:
-              fieldNames = definition.fields.map(f => f.name.value)
-              fields = (typeMapEntry as GraphQLObjectType | GraphQLInterfaceType).getFields()
-              keysFn = (x: GraphQLFieldMap<any, any>) => Object.keys(x)
-              deleteFn = (coll: GraphQLFieldMap<any, any>, item) => delete coll[item]
-              break
-            case ENUM_TYPE_DEFINITION:
-              fieldNames = definition.values.map(f => f.name.value)
-              fields = (typeMapEntry as GraphQLEnumType).getValues()
-              keysFn = (x: GraphQLEnumValue[]) => x.map(f => f.name)
-              deleteFn = (coll: GraphQLEnumValue[], item) =>
-                remove(coll, (f: GraphQLEnumValue) => f.name === item)
-              break
-            case UNION_TYPE_DEFINITION:
-              fieldNames = definition.types.map(f => f.name.value)
-              fields = (typeMapEntry as GraphQLUnionType).getTypes()
-              keysFn = (x: GraphQLObjectType[]) => x.map(f => f.name)
-              deleteFn = (coll: GraphQLObjectType[], item) => remove(coll, f => f.name === item)
-              break
-            case INPUT_OBJECT_TYPE_DEFINITION:
-              fieldNames = definition.fields.map(f => f.name.value)
-              fields = (typeMapEntry as GraphQLInputObjectType).getFields()
-              keysFn = (x: GraphQLInputFieldMap) => Object.keys(x)
-              deleteFn = (coll: GraphQLInputFieldMap, item) => delete coll[item]
-              break
-            default:
-              throw new Error(`Unsupported definition type found in schema: '${definition.kind}'`)
-          }
+        // Merge, taking the definitions from the final schema.
+        // Unfortunately, this breaks resolvers (https://github.com/apollographql/graphql-tools/issues/504)
+        // And it doesn't apply onTypeConflict on the entire Query/Mutation types, just to individual fields
+        const schemaDef: GraphQLSchema = mergeSchemas({
+          schemas: [req.qewl.schemas.mergedSchema, finalSchema],
+          onTypeConflict: (l, r) => r
+        }) as GraphQLSchema
 
-          for (const field of keysFn(fields)) {
-            if (!fieldNames.includes(field)) {
-              deleteFn(fields, field)
+        // Restore the resolvers for the query type, or remove it or any of its fields if not part of the final schema
+        const queryType = schemaDef.getQueryType()
+        if (queryType) {
+          if (!doc.definitions.map(d => (d as TypeDefinitionNode).name.value).includes(queryType.name)) {
+            delete (schemaDef as any)._queryType
+          } else {
+            const queryFields = queryType.getFields()
+            const queryFieldKeys = Object.keys(queryFields)
+            const newQueryTypeFields = (doc.definitions.find(
+              d => (d as TypeDefinitionNode).name.value === queryType.name
+            ) as any).fields.map((f: any) => f.name.value)
+            for (const key of queryFieldKeys) {
+              if (newQueryTypeFields.includes(key)) {
+                queryFields[key].resolve = req.qewl.schemas.mergedSchema.getQueryType().getFields()[
+                  key
+                ].resolve
+              } else {
+                delete schemaDef.getQueryType().getFields()[key]
+              }
             }
           }
         }
 
-        const mutationType = schemaDef.getMutationType().name
-        if (!doc.definitions.map(d => (d as TypeDefinitionNode).name.value).includes(mutationType)) {
-          delete (schemaDef as any)._mutationType
+        // Restore the resolvers for the mutation type, or remove it or any of its fields if not part of the final schema
+        const mutationType = schemaDef.getMutationType()
+        if (mutationType) {
+          if (!doc.definitions.map(d => (d as TypeDefinitionNode).name.value).includes(mutationType.name)) {
+            delete (schemaDef as any)._mutationType
+          } else {
+            const mutationFields = mutationType.getFields()
+            const mutationFieldKeys = Object.keys(mutationFields)
+            const newMutationTypeFields = (doc.definitions.find(
+              d => (d as TypeDefinitionNode).name.value === mutationType.name
+            ) as any).fields.map((f: any) => f.name.value)
+            for (const key of mutationFieldKeys) {
+              if (newMutationTypeFields.includes(key)) {
+                mutationFields[key].resolve = req.qewl.schemas.mergedSchema.getMutationType().getFields()[
+                  key
+                ].resolve
+              } else {
+                delete schemaDef.getMutationType().getFields()[key]
+              }
+            }
+          }
         }
 
-        const queryType = schemaDef.getQueryType().name
-        if (!doc.definitions.map(d => (d as TypeDefinitionNode).name.value).includes(queryType)) {
-          delete (schemaDef as any)._queryType
+        // Restore the resolvers for the subscription type, or remove it or any of its fields if not part of the final schema
+        const subscriptionType = schemaDef.getSubscriptionType()
+        if (subscriptionType) {
+          if (
+            !doc.definitions.map(d => (d as TypeDefinitionNode).name.value).includes(subscriptionType.name)
+          ) {
+            delete (schemaDef as any)._subscriptionType
+          } else {
+            const subscriptionFields = subscriptionType.getFields()
+            const subscriptionFieldKeys = Object.keys(subscriptionFields)
+            const newSubscriptionTypeFields = (doc.definitions.find(
+              d => (d as TypeDefinitionNode).name.value === subscriptionType.name
+            ) as any).fields.map((f: any) => f.name.value)
+            for (const key of subscriptionFieldKeys) {
+              if (newSubscriptionTypeFields.includes(key)) {
+                subscriptionFields[
+                  key
+                ].resolve = req.qewl.schemas.mergedSchema.getSubscriptionType().getFields()[key].resolve
+              } else {
+                delete schemaDef.getSubscriptionType().getFields()[key]
+              }
+            }
+          }
         }
+
         req.qewl.schemas.finalSchema = schemaDef
         put(`qewl.finalSchema`, schemaDef)
       } else {
